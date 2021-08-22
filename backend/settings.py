e42e80f004e616ed641615312d0542200d76b742
BeingPratham <upadhyayprathamm215@gmail.com>
2021-08-22 17:31:39 +0530

    finally ig

--- backend/settings.py
+++ backend/settings.py
@@ -69,7 +69,7 @@ ROOT_URLCONF = 'backend.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, ('build'))],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -135,6 +135,9 @@ USE_TZ = True
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, ('build/static'))
+]
 DISABLE_COLLECTSTATIC=1
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
--- backend/urls.py
+++ backend/urls.py
@@ -15,8 +15,8 @@ Including another URLconf
 """
 
 from django.contrib import admin
-from django.urls import path,include
-
+from django.urls import path,include,re_path
+from django.views.generic import TemplateView
 from articles.api import views;
 
 
@@ -27,5 +27,5 @@ urlpatterns = [
     path('rest-auth/registration/', include('rest_auth.registration.urls')),
     path('rest-auth/', include('rest_auth.urls')),
     path('api/', include('articles.api.urls')),
-    
+    re_path('.*', TemplateView.as_view(template_name='index.html'))
 ]
